e30538edc1a0ccc0d3f5d1964a02636d697a649b
Fedor Bokov <fedor@quasi.ai>
2024-06-11 13:14:06 -0400

    print errno when SCHED_FIFO policy cannot be enabled

--- controller_manager/src/ros2_control_node.cpp
+++ controller_manager/src/ros2_control_node.cpp
@@ -52,10 +52,10 @@ int main(int argc, char ** argv)
       {
         RCLCPP_WARN(
           cm->get_logger(),
-          "Could not enable FIFO RT scheduling policy. Consider setting up your user to do FIFO RT "
+          "Could not enable FIFO RT scheduling policy: %i. Consider setting up your user to do FIFO RT "
           "scheduling. See "
           "[https://control.ros.org/master/doc/ros2_control/controller_manager/doc/userdoc.html] "
-          "for details.");
+          "for details.", (int)errno);
       }
 
       rclcpp::Rate loop_rate(cm->get_update_rate());
